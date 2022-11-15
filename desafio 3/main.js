66b5afb0fc86a5904c0c95db89d46d9d28e4acf5
admin <pedropaulo@usa.com>
2022-11-15 17:43:03 -0300

    primeiro commit

---
+++ desafio 3/main.js
@@ -0,0 +1,12 @@
+const lista = document.querySelector("ul");
+console.log(lista)
+document.querySelector("#botao").addEventListener("click", () => {
+  lista.setAttribute("data-lista", "mostrar");
+});
+
+document.querySelector(".close").addEventListener("click", () => {
+  lista.setAttribute("data-lista", "esconder");
+});
+
+
+
